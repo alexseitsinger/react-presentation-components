d2c930ec97838e0575245b625d145ec11fc1a27c
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2020-03-02 20:18:22 -0500

    refactor: Adds width:100%.

--- src/wrapper/elements.ts
+++ src/wrapper/elements.ts
@@ -6,6 +6,7 @@ interface WrapperContainerProps {
 }
 
 export const WrapperContainer = styled.div`
+  width: 100%;
   max-width: 1200px;
   margin: 0 auto;
   ${({ minHeight }: WrapperContainerProps): any => {
