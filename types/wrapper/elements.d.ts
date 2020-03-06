7ed3e5518f34e555c835244eafee67b2918eb40c
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2020-03-06 17:22:00 -0500

    refactor: Updates types.

--- types/wrapper/elements.d.ts
+++ types/wrapper/elements.d.ts
@@ -1,7 +1,8 @@
 /// <reference types="react" />
 /// <reference types="@emotion/core" />
-interface WrapperContainerProps {
+interface Props {
     minHeight?: string;
+    maxWidth?: string;
 }
-export declare const WrapperContainer: import("@emotion/styled-base").StyledComponent<import("react").DetailedHTMLProps<import("react").HTMLAttributes<HTMLDivElement>, HTMLDivElement>, WrapperContainerProps, object>;
+export declare const WrapperElement: import("@emotion/styled-base").StyledComponent<import("react").DetailedHTMLProps<import("react").HTMLAttributes<HTMLDivElement>, HTMLDivElement>, Props, object>;
 export {};
--- types/wrapper/index.d.ts
+++ types/wrapper/index.d.ts
@@ -1,9 +1,10 @@
 import React, { ReactNode } from "react";
 import { CSSObject } from "@emotion/core";
-interface WrapperProps {
-    containerStyle?: CSSObject;
+interface Props {
     children: ReactNode | ReactNode[];
+    containerStyle?: CSSObject;
     minHeight?: string;
+    maxWidth?: string;
 }
-export declare const Wrapper: ({ children, containerStyle, minHeight, }: WrapperProps) => React.ReactElement<any, string | ((props: any) => React.ReactElement<any, string | any | (new (props: any) => React.Component<any, any, any>)> | null) | (new (props: any) => React.Component<any, any, any>)>;
+export declare const Wrapper: ({ children, containerStyle, minHeight, maxWidth, }: Props) => React.ReactElement<any, string | ((props: any) => React.ReactElement<any, string | any | (new (props: any) => React.Component<any, any, any>)> | null) | (new (props: any) => React.Component<any, any, any>)>;
 export {};
